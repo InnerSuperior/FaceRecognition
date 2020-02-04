7a7240d69842b9fd353ee2d0ac116daec76d445d
InnerSuperior <avsaiharish@gmail.com>
2020-02-04 20:54:14 +0530

    commit

--- facematch.py
+++ facematch.py
@@ -1,7 +1,7 @@
 import subprocess
 
 def match():
-    p = subprocess.run('face_recognition --tolerance 0.5 --cpus 8 KNOWN_PEOPLE_FOLDER unknown ',shell = True, capture_output = True)
+    p = subprocess.run('face_recognition --tolerance 0.5 --cpus 8 known unknown ',shell = True, capture_output = True)
     print (p.stdout)
 
 
